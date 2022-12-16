adb5ef81903035f3d21f04ce614043017a8cbbb2
mounikajayavaram <120085351+mounikajayavaram@users.noreply.github.com>
2022-12-16 15:45:06 +0530

    first commit

---
+++ mybooks/book/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import * 
+
+urlpatterns = [
+    path("",Booklist),
+    path("add/",post_book),
+    path("update/<int:id>/",update_book),
+    path("delete/<int:id>/",delete_book),
+]
---
+++ mybooks/book/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from .serializers import*
+from .models import*
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Create your views here.
+#read
+@api_view(['GET'])
+def Booklist(request):
+    booksobj=BooksModel.objects.all()
+    serializer= BookSerializer(booksobj,many=True)
+    return Response(serializer.data)
+
+    #create
+    #reead
+@api_view(['POST'])
+def post_book(request):
+    booksobj=BooksModel.objects.all()
+    serializer= BookSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data) 
+
+#update
+@api_view(['POST'])
+def update_book(request,id):
+    booksobj=BooksModel.objects.get(id=id)
+    serializer= BookSerializer(instance=booksobj,data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+#delete
+@api_view(['DELETE'])
+def delete_book(request,id):
+    booksobj=BooksModel.objects.get(id=id)
+    booksobj.delete()
+    return Response("book is deleted")                      
